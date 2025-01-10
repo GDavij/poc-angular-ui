ba62d11bfe2d56789a67f309b00a89cf3dba5926
Gabriel Jacobini <gdavijac@gmail.com>
2025-01-10 00:37:20 -0300

    feat(basic member endpoints): add basic members endpoints

---
+++ backend/main.ts
@@ -0,0 +1,13 @@
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
